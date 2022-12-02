7ed6584d6062b8b4fadc7d0aee6d6dcb90beffc7
Jason Silvaroli <jsilvaroli@myseneca.ca>
2022-12-02 16:03:21 -0500

    updated index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,46 @@
-import { Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
     return (
-        <div className={styles.container} id="test">
-            <div style={{ opacity: 1 }} id="main">
-                <a id="home-section"></a>
-                <Grid
-                    container
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    sx={{ marginTop: "25rem" }}
-                >
-                    <Grid item>
-                        <Typography
-                            textAlign={"center"}
-                            variant="h2"
-                            letterSpacing={10}
-                            fontWeight="400"
-                            color={"#FBFAF5"}
-                        >
-                            Kevin Lin
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography
-                            color={"#FBFAF5"}
-                            textAlign={"center"}
-                            variant="h6"
-                            letterSpacing={5}
-                            fontWeight="300"
-                        >
-                            Creative Advertising
-                        </Typography>
-                    </Grid>
+        <Container id="main">
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ marginTop: "25rem" }}
+            >
+                <Grid item>
+                    <Typography
+                        textAlign={"center"}
+                        variant="h2"
+                        letterSpacing={10}
+                        fontWeight="400"
+                        color={"#FBFAF5"}
+                    >
+                        Kevin Lin
+                    </Typography>
                 </Grid>
-                <Typography
-                    color={"#FBFAF5"}
-                    textAlign={"center"}
-                    marginTop="20rem"
-                >
-                    Contact Me
-                </Typography>
-            </div>
-        </div>
+                <Grid item>
+                    <Typography
+                        color={"#FBFAF5"}
+                        textAlign={"center"}
+                        variant="h6"
+                        letterSpacing={5}
+                        fontWeight="300"
+                    >
+                        Creative Advertising
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Typography
+                color={"#FBFAF5"}
+                textAlign={"center"}
+                sx={{ marginTop: { xs: "0rem", md: "20rem" } }}
+            >
+                Contact Me
+            </Typography>
+        </Container>
     );
 }
